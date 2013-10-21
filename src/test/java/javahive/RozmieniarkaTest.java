b72b15e5bce2bee68fbffba3bff203bca87b8b1c
Marcin <mjg@g.pl>
2013-10-21 16:22:06 +0200

    poprawnie utworzony test dla kwoty 124 zł i "polskich" nominałów

--- src/test/java/javahive/RozmieniarkaTest.java
+++ src/test/java/javahive/RozmieniarkaTest.java
@@ -1,11 +1,20 @@
 package javahive;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RozmieniarkaTest {
 	private final double[] NOMINALY_PL = { 200, 100, 50, 20, 10, 5, 2, 1, .50, .20, .10, .05, .02, 0.01 };
 	@Test
-	public void PowinienRozmieniac(){
-		System.out.println(new RozmieniarkaImpl().rozmien(34.43,NOMINALY_PL));
+	public void PowinienRozmieniacPoprawnie124(){
+		//given
+		Rozmieniarka rozmieniarka=new RozmieniarkaImpl();
+		//when
+		Nominaly nominaly=rozmieniarka.rozmien(124, NOMINALY_PL);
+		//then
+		Assert.assertTrue(nominaly.getIlosc(100)==1);
+		Assert.assertTrue(nominaly.getIlosc(20)==1);
+		Assert.assertTrue(nominaly.getIlosc(2)==2);
+		
 	}
 }
